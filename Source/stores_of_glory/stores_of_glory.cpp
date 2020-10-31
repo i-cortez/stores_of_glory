dda5e6e0818f84b361308bb873f64ce84c69a7d4
Izze <56184561+i-cortez@users.noreply.github.com>
2020-10-30 20:47:58 -0700

    Preparing to init new project

--- Source/stores_of_glory/stores_of_glory.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "stores_of_glory.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, stores_of_glory, "stores_of_glory" );
- 
